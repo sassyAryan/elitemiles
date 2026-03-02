ba0f87a79207c1c3aaa4e291f4487bbdf803a6d0
sassyAryan <126312268+sassyAryan@users.noreply.github.com>
2026-03-02 13:12:05 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Set Launch Date (Change this to your real launch date)
+const launchDate = new Date("April 1, 2026 00:00:00").getTime();
+
+const countdownFunction = setInterval(function () {
+
+    const now = new Date().getTime();
+    const distance = launchDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(countdownFunction);
+        document.getElementById("countdown").innerHTML = "We Are Live!";
+    }
+
+}, 1000);
